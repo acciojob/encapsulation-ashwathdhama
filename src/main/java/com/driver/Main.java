839d8a8d4e2e1d850de5fd91cc56c9eceb29061c
sanyam raj <sanyamraj007@gmail.com>
2023-06-10 21:27:34 +0530

    solution added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly e = new RWOnly();
+        e.setName("Sarthak");
+        System.out.println(e.getName());
+    }
 }
